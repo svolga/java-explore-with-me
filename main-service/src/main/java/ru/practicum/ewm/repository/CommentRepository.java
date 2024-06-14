b0d4ffd3d7255dd59f02c0059f8c2f22b877b4b0
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-06-14 19:55:40 +0300

    feat: Добавить комментарии к событиям

--- main-service/src/main/java/ru/practicum/ewm/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/CommentRepository.java
@@ -8,7 +8,10 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     boolean existsCommentByIdAndUserId(Long id, Long userId);
+
     List<Comment> findCommentsByEvent_Id(Long userId, Pageable pageable);
+
     List<Comment> findCommentsByUser_Id(Long userId, Pageable pageable);
+
     List<Comment> findCommentsByUser_IdAndEvent_Id(Long userId, Long eventId, Pageable pageable);
 }
